9dc10febecbdfa7eeb052b8ae062fdd1bc2ebe23
Aakash Goel <goelaakash79@gmail.com>
2019-03-19 17:58:41 +0530

    db connection added | smaller updates

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
+const app = express();
 
 const indexRouter = require('./routes/index');
 
-const app = express();
+require('dotenv').config();
+require('./config/dbconnection');
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,4 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.listen(3000);
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+        console.log("Error in running server");
+        return;
+    }
+    console.log(`Server is up and running on http://localhost:${process.env.PORT}`);
+});
