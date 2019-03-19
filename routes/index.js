c6acb2e65e5b9d540bac78b88ac49056480751f3
Aakash Goel <goelaakash79@gmail.com>
2019-03-19 20:23:11 +0530

    static ui completed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', (req, res) => {
+  console.log(req.post)
 });
 
 module.exports = router;
