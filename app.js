e2e082160d5e0e7094ba05c80a8644325a0c065a
Aakash Goel <goelaakash79@gmail.com>
2019-03-19 23:34:40 +0530

    auth added

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
 
 const indexRouter = require('./routes/index');
+const User = require('./models/User');
 
 require('dotenv').config();
 require('./config/dbconnection');
 
 app.use(cors());
+app.use(cookieParser());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,9 +26,36 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    saveUninitialized: false,
+    resave: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        let namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,102 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 router.get('/', (req, res) => {
-  res.redirect('/login');
+    res.redirect('/login');
 });
 
 router.get('/login', (req, res) => {
-  res.render('login');
+    res.render('login');
 });
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/feed',
+    failureRedirect: '/login'
+}), function (req, res) {
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.render('login', {message: 'You are logged out successfully'});
+});
+
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.render('login', {message: 'Login Again!'});
+}
 
 router.get('/register', (req, res) => {
-  res.render('register');
+    res.render('register');
 });
 
-router.get('/feed', (req, res) => {
-  res.render('index', { title: 'Express' });
+router.post('/register', (req, res) => {
+    User.findOne({email: req.body.email}, (err, exists) => {
+        if (err) throw err;
+        if (exists) {
+            res.render('register', {errors: [{msg: 'User already exists, try using different Email Id.'}]});
+        } else {
+
+            bcrypt.genSalt(10, function (err, salt) {
+                bcrypt.hash(req.body.password, salt, function (err, hash) {
+                    password = hash;
+                    let user_data = {
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: password
+                    };
+                    User.create(user_data, (err, userData) => {
+                        if (err) throw err;
+                        res.render('login', {message: 'You are registered, please verify your email, then login.'});
+                    })
+                });
+            });
+        }
+    });
+})
+;
+
+router.get('/feed', isAuthenticated, (req, res) => {
+    // console.log(req.user);
+    res.render('index', {username: req.user.username});
 });
 
-router.post('/feed', (req, res) => {
-  console.log(req.post);
+router.post('/feed', isAuthenticated, (req, res) => {
+    console.log(req.post);
+    res.send("hi");
+});
+
+passport.use(new LocalStrategy(function (username, password, done) {
+    let pass = password;
+    User.findOne({username: username}, (err, userData) => {
+        if (err) throw err;
+        if (!userData) {
+            return done(null, false, {message: 'Unknown User'});
+        }
+        let hash = userData.password;
+        bcrypt.compare(pass, hash, function (err, isMatch) {
+            if (err) throw err;
+            if (isMatch) {
+                return done(null, userData);
+            } else {
+                return done(null, false, {message: 'Invalid password'});
+            }
+        });
+    });
+}));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
 });
 
 module.exports = router;
